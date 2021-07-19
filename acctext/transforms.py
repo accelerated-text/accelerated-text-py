bf696606ee99de1f9302775fb9b64d8f075d1c46
Rokas Ramanauskas <rokas.ramanauskas@tokenmill.lt>
2021-07-19 12:20:30 +0300

    fix: do not apply transform when dict item does not exist

--- acctext/transforms.py
+++ acctext/transforms.py
@@ -7,4 +7,4 @@ def dictionary_item(x: Dict):
             "forms": [phrase.get('text') for phrase in x.get('phrases', [])],
             "category": x.get('partOfSpeech'),
             "language": x.get('language'),
-            "attributes": {attr.get('name'): attr.get('value') for attr in x.get('attributes', [])}}
+            "attributes": {attr.get('name'): attr.get('value') for attr in x.get('attributes', [])}} if x else x
